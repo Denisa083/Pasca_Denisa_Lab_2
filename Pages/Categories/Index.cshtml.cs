887f928357c22cc0ee470e508f7e37b745c168c0
Denisa Pasca <denisa.pasca083@gmail.com>
2022-11-21 19:05:00 +0200

    aw

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -22,11 +22,25 @@ namespace Pasca_Denisa_Lab_2.Pages.Categories
 
         public IList<Category> Category { get; set; } = default!;
 
-        public async Task OnGetAsync()
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            if (_context.Category != null)
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+            .Include(i => i.BookCategories)
+            .ThenInclude(c => c.Book)
+            .ThenInclude(c => c.Author)
+            .OrderBy(i => i.CategoryName)
+            .ToListAsync();
+            if (id != null)
             {
-                Category = await _context.Category.ToListAsync();
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                .Where(i => i.ID == id.Value).Single();
+                CategoryData.BookCategories = category.BookCategories;
             }
         }
     }
